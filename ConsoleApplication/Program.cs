34bb5d11e28bd5b3af86a66cef69f99a7ca8802f
rahulroy9202 <rahulroy9202@gmail.com>
2013-09-15 21:14:27 +0530

    solution files upadted

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -242,3 +242,36 @@ mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
 mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
 }
 */
+
+/*/// <summary>
+/// Struct representing a point.
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+public struct POINT
+{
+    public int X;
+    public int Y;
+
+    public static implicit operator Point(POINT point)
+    {
+        return new Point(point.X, point.Y);
+    }
+}
+
+/// <summary>
+/// Retrieves the cursor's position, in screen coordinates.
+/// </summary>
+/// <see>See MSDN documentation for further information.</see>
+[DllImport("user32.dll")]
+public static extern bool GetCursorPos(out POINT lpPoint);
+
+public static Point GetCursorPosition()
+{
+    POINT lpPoint;
+    GetCursorPos(out lpPoint);
+    //bool success = User32.GetCursorPos(out lpPoint);
+    // if (!success)
+
+    return lpPoint;
+}
+*/
